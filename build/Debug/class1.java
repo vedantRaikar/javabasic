27346891a069599c5cd36658c6ad7353f96540c1
vedantRaikar <vedantraikar117@gmail.com>
2023-09-06 15:23:04 +0530

    exception

---
+++ build/Debug/class1.java
@@ -0,0 +1,13 @@
+package build.Debug;
+
+public class class1 {
+    public static void main(String [] args){
+        try{
+        int d = 0 ;
+        int c = 42 / d;}
+        catch(ArithmeticException e){
+            System.out.println("you cant divide by 0");
+
+        }
+    }
+}
